a970a2c214ae209dd569e1475385cfe78681a437
Pedro Gomes <pedrogomes94@gmail.com>
2019-07-10 19:12:43 +0100

    fix onclick

--- pretix_eth/static/pretix_eth/connect.js
+++ pretix_eth/static/pretix_eth/connect.js
@@ -1,103 +1,111 @@
 "use strict";
+document.addEventListener("DOMContentLoaded", function() {
+  const WalletConnect = window.WalletConnect.default;
+  const qrcode = window.WalletConnectQRCodeModal.default;
+  const Web3 = window.Web3;
 
-const WalletConnect = window.WalletConnect.default;
-const qrcode = window.WalletConnectQRCodeModal.default;
-const Web3 = window.Web3;
-
-async function onMetaMask() {
-  clearError();
-  const provider = await initMetaMask();
-  const web3 = new Web3(provider);
-  const networkId = await web3.eth.getNetwork();
-  if (networkId !== 1) {
-    displayError("Please switch to Ethereum Mainnet");
-    return;
-  }
-  const accounts = await web3.eth.getAccounts();
-  const from = accounts[0];
-  const tx = await formatTransaction(
-    from,
-    window.to,
-    window.amount,
-    window.currency
-  );
-  const txhash = await web3.eth.sendTransaction(tx);
-  return txhash;
-}
-
-async function onWalletConnect() {
-  clearError();
-  const wc = await initWalletConnect();
-  const chainId = wc.chainId;
-  if (chainId !== 1) {
-    displayError("Please switch to Ethereum Mainnet");
-    return;
-  }
-  const accounts = wc.accounts;
-  const from = accounts[0];
-  const tx = await formatTransaction(
-    from,
-    window.to,
-    window.amount,
-    window.currency
-  );
-  const txhash = await wc.sendTransaction(tx);
-  return txhash;
-}
-
-async function initMetaMask() {
-  let provider = null;
-  if (window.ethereum) {
-    provider = window.ethereum;
-    try {
-      await window.ethereum.enable();
-    } catch (error) {
-      throw new Error("User Rejected");
+  async function onMetaMask() {
+    clearError();
+    const provider = await initMetaMask();
+    const web3 = new Web3(provider);
+    const networkId = await web3.eth.getNetwork();
+    if (networkId !== 1) {
+      displayError("Please switch to Ethereum Mainnet");
+      return;
     }
-  } else if (window.web3) {
-    provider = window.web3.currentProvider;
-  } else {
-    throw new Error("No Web3 Provider found");
+    const accounts = await web3.eth.getAccounts();
+    const from = accounts[0];
+    const tx = await formatTransaction(
+      from,
+      window.to,
+      window.amount,
+      window.currency
+    );
+    const txhash = await web3.eth.sendTransaction(tx);
+    return txhash;
   }
-  return provider;
-}
 
-function initWalletConnect() {
-  return new Promise(async (resolve, reject) => {
-    const wc = new WalletConnect({
-      bridge: "https://bridge.walletconnect.org"
-    });
+  async function onWalletConnect() {
+    clearError();
+    const wc = await initWalletConnect();
+    const chainId = wc.chainId;
+    if (chainId !== 1) {
+      displayError("Please switch to Ethereum Mainnet");
+      return;
+    }
+    const accounts = wc.accounts;
+    const from = accounts[0];
+    const tx = await formatTransaction(
+      from,
+      window.to,
+      window.amount,
+      window.currency
+    );
+    const txhash = await wc.sendTransaction(tx);
+    return txhash;
+  }
 
-    if (!wc.connected) {
-      await wc.createSession();
-      qrcode.open(wc.uri, () => {
-        console.log("QR Code Modal closed");
-      });
+  async function initMetaMask() {
+    let provider = null;
+    if (window.ethereum) {
+      provider = window.ethereum;
+      try {
+        await window.ethereum.enable();
+      } catch (error) {
+        throw new Error("User Rejected");
+      }
+    } else if (window.web3) {
+      provider = window.web3.currentProvider;
     } else {
-      resolve(wc);
+      throw new Error("No Web3 Provider found");
     }
+    return provider;
+  }
 
-    wc.on("connect", (error, payload) => {
-      if (error) {
-        reject(error);
+  function initWalletConnect() {
+    return new Promise(async (resolve, reject) => {
+      const wc = new WalletConnect({
+        bridge: "https://bridge.walletconnect.org"
+      });
+
+      if (!wc.connected) {
+        await wc.createSession();
+        qrcode.open(wc.uri, () => {
+          console.log("QR Code Modal closed");
+        });
+      } else {
+        resolve(wc);
       }
 
-      qrcode.close();
+      wc.on("connect", (error, payload) => {
+        if (error) {
+          reject(error);
+        }
 
-      resolve(wc);
+        qrcode.close();
+
+        resolve(wc);
+      });
     });
-  });
-}
+  }
 
-const ERROR_ELM_ID = "connect-error-message";
+  const ERROR_ELM_ID = "connect-error-message";
+  const METAMASK_ID = "connect-metamask";
+  const WALLETCONNECT_ID = "connect-walletconnect";
 
-function clearError() {
-  const el = document.getElementById(ERROR_ELM_ID);
-  el.innerHTML = "";
-}
+  function clearError() {
+    const el = document.getElementById(ERROR_ELM_ID);
+    el.innerHTML = "";
+  }
 
-function displayError(message) {
-  const el = document.getElementById(ERROR_ELM_ID);
-  el.innerHTML = message;
-  console.error(message);
-}
+  function displayError(message) {
+    const el = document.getElementById(ERROR_ELM_ID);
+    el.innerHTML = message;
+    console.error(message);
+  }
+
+  document.getElementById(METAMASK_ID).addEventListener("click", onMetaMask);
+  document
+    .getElementById(WALLETCONNECT_ID)
+    .addEventListener("click", onWalletConnect);
+});
