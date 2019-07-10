9eeadb94d68c025a335c83b5171104f9c54b9510
Pedro Gomes <pedrogomes94@gmail.com>
2019-07-10 20:05:32 +0100

    one last try

--- pretix_eth/static/pretix_eth/connect.js
+++ pretix_eth/static/pretix_eth/connect.js
@@ -3,36 +3,36 @@ document.addEventListener("DOMContentLoaded", function() {
   const Web3Connect = window.Web3Connect.default;
   const Web3 = window.Web3;
 
-  async function onConnect(e) {
-    e.preventDefault();
+  async function onConnect(type) {
     clearError();
 
-    const web3Connect = new Web3Connect.Core();
+    const connector =
+      type === "METAMASK"
+        ? Web3Connect.ConnectToInjected
+        : Web3Connect.ConnectToWalletConnect;
 
-    web3Connect.on("connect", async provider => {
-      const web3 = new Web3(provider);
-      const networkId = await web3.eth.net.getId();
-      if (networkId !== 1) {
-        displayError("Please switch to Ethereum Mainnet");
-        return;
-      }
-      const accounts = await web3.eth.getAccounts();
-      const from = accounts[0];
-      const tx = await formatTransaction(
-        from,
-        window.to,
-        window.amount,
-        window.currency
-      );
-      const txhash = await web3.eth.sendTransaction(tx);
-      return txhash;
-    });
-
-    web3Connect.toggleModal();
+    const provider = await connector();
+    const web3 = new Web3(provider);
+    const networkId = await web3.eth.net.getId();
+    if (networkId !== 1) {
+      displayError("Please switch to Ethereum Mainnet");
+      return;
+    }
+    const accounts = await web3.eth.getAccounts();
+    const from = accounts[0];
+    const tx = await formatTransaction(
+      from,
+      window.to,
+      window.amount,
+      window.currency
+    );
+    const txhash = await web3.eth.sendTransaction(tx);
+    return txhash;
   }
 
   const ERROR_ELM_ID = "connect-error-message";
-  const BUTTON_ID = "connect-button";
+  const METAMASK_ID = "connect-metamask";
+  const WALLETCONNECT_ID = "connect-walletconnect";
 
   function clearError() {
     const el = document.getElementById(ERROR_ELM_ID);
@@ -45,5 +45,12 @@ document.addEventListener("DOMContentLoaded", function() {
     console.error(message);
   }
 
-  document.getElementById(BUTTON_ID).addEventListener("click", onConnect);
+  document.getElementById(METAMASK_ID).addEventListener("click", e => {
+    e.preventDefault();
+    onConnect("METAMASK");
+  });
+  document.getElementById(WALLETCONNECT_ID).addEventListener("click", e => {
+    e.preventDefault();
+    onConnect("WALLETCONNECT");
+  });
 });
