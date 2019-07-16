139385fc6816e2820b5574ef614f59925b51e769
ligi <ligi@ligi.de>
2019-07-16 17:03:54 +0200

    Bump version to 1.0.3

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ extras_require['dev'] = (
 
 setup(
     name='pretix-eth-payment-plugin',
-    version='1.0.2',
+    version='1.0.3',
     description='Ethereum payment provider plugin for pretix software',
     long_description=long_description,
     long_description_content_type='text/markdown',
