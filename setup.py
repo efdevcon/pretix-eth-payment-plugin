3e2aba392c7bbeef27c805caa610047b815068dd
Jakub Vysoky <jakub@borka.cz>
2020-04-21 11:22:48 +0200

    mark development version

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ extras_require['dev'] = (
 
 setup(
     name='pretix-eth-payment-plugin',
-    version='1.1.2',
+    version='2.0.0-dev',
     description='Ethereum payment provider plugin for pretix software',
     long_description=long_description,
     long_description_content_type='text/markdown',
