479e2f7e641ab84ae39fd4d53d729dc548a34ac3
Jakub Vysoky <jakub@borka.cz>
2020-04-22 11:58:02 +0200

    auto filling receiving address question plugin
    listen on two pretix signals
    inject custom javascript
    inject custom form field with info about other fields

--- pretix_eth/signals.py
+++ pretix_eth/signals.py
@@ -1,10 +1,44 @@
 from django.dispatch import receiver
 from django.urls import resolve, reverse
 from django.utils.translation import gettext_lazy as _
+from django.template.loader import get_template
+from django import forms
 
+from pretix.presale.signals import html_head, question_form_fields
 from pretix.base.signals import register_payment_providers
 from pretix.control.signals import nav_event_settings
 
+
+NFT_QUESTION_IDENTIFIER = 'eth-payment-plugin-nft-address'
+PAYMENT_ETH_INFO_ID = 'payment_eth_info'
+PAYMENT_ETH_INFO_NAME = 'payment_eth_info'
+
+
+@receiver(html_head, dispatch_uid="payment_eth_add_question_type_javascript")
+def add_question_type_javascript(sender, request, **kwargs):
+    url = resolve(request.path_info) # TODO: enable js only when question is asked
+    template = get_template('pretix_eth/question_type_javascript.html')
+    context = {
+        'event': sender,
+    }
+    return template.render(context)
+
+@receiver(question_form_fields, dispatch_uid="payment_eth_mark_question_type")
+def mark_question_type(sender, position, **kwargs):
+    questions = sender.questions.filter(identifier=NFT_QUESTION_IDENTIFIER)
+    payment_eth_info_widget = forms.TextInput(attrs={
+        'value': ','.join( str(q.id) for q in questions ),
+        'id': PAYMENT_ETH_INFO_ID,
+    })
+    payment_eth_info_field = forms.CharField(
+        label='Payment ETH Info',
+        max_length=100,
+        widget=payment_eth_info_widget,
+        required=False,
+    )
+    return {
+        PAYMENT_ETH_INFO_NAME: payment_eth_info_field,
+    }
 
 @receiver(register_payment_providers, dispatch_uid="payment_eth")
 def register_payment_provider(sender, **kwargs):
---
+++ pretix_eth/static/pretix_eth/question-type.js
@@ -0,0 +1,7 @@
+// payment_eth_html_add_question_type_javascript
+$(function () {
+    // there is `payment_eth_info` hidden input with list of IDs
+    // of the other fields we want to change functionality
+    console.log($('#payment_eth_info').val());
+});
+
