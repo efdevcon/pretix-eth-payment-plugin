b7b5a46f5da63063016a9b821861cff5de668434
Jakub Vysoky <jakub@borka.cz>
2020-04-28 21:39:43 +0200

    add allowed stuff to CSP

--- pretix_eth/signals.py
+++ pretix_eth/signals.py
@@ -4,7 +4,12 @@ from django.utils.translation import gettext_lazy as _
 from django.template.loader import get_template
 from django import forms
 
-from pretix.presale.signals import html_head, question_form_fields
+from pretix.base.middleware import _parse_csp, _merge_csp, _render_csp
+from pretix.presale.signals import (
+    html_head,
+    question_form_fields,
+    process_response,
+)
 from pretix.base.signals import register_payment_providers
 from pretix.control.signals import nav_event_settings
 
@@ -13,6 +18,22 @@ NFT_QUESTION_IDENTIFIER = 'eth-payment-plugin-nft-address'
 PAYMENT_ETH_INFO_CLASS = 'payment_eth_info'
 PAYMENT_ETH_INFO_NAME = 'payment_eth_info'
 
+
+@receiver(process_response, dispatch_uid="payment_eth_add_question_type_csp")
+def signal_process_response(sender, request, response, **kwargs):
+    url = resolve(request.path_info) # TODO: enable js only when question is asked
+    h = {}
+    if 'Content-Security-Policy' in response:
+        h = _parse_csp(response['Content-Security-Policy'])
+    _merge_csp(h, {
+        'style-src': [
+            "'sha256-47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU='",
+            "'sha256-O+AX3tWIOimhuzg+lrMfltcdtWo7Mp2Y9qJUkE6ysWE='",
+        ],
+        'manifest-src': ["'self'"],
+    })
+    response['Content-Security-Policy'] = _render_csp(h)
+    return response
 
 @receiver(html_head, dispatch_uid="payment_eth_add_question_type_javascript")
 def add_question_type_javascript(sender, request, **kwargs):
@@ -23,7 +44,7 @@ def add_question_type_javascript(sender, request, **kwargs):
     }
     return template.render(context)
 
-@receiver(question_form_fields, dispatch_uid="payment_eth_mark_question_type")
+@receiver(question_form_fields, dispatch_uid="payment_eth_add_question_type_form_field")
 def mark_question_type(sender, position, **kwargs):
     questions = sender.questions.filter(identifier=NFT_QUESTION_IDENTIFIER)
     question_ids = [ 'id_{p.id}-question_{q.id}'.format(p=position, q=q) for q in questions ]
