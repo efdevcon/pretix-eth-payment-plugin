ce5d89453dbab42404e3817fedb47c8d6bdb932d
David Sanders <david.sanders@unsupervised.com>
2020-05-04 10:48:24 -0600

    Fix conflict for more recent versions of pretix

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
     author_email='pretix-eth-payment-plugin@ethereum.org',
     license='Apache Software License',
     install_requires=[
-        "Django>=2.2.0,<2.3.0",
+        "Django>=2.2.0",
         "pretix>=2.8.2",
         "web3>=5.7.0",
         "eth-abi>=2.1.1,<3",
