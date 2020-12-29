76741c2ffd0f75a2ce40fc5f0f81f72439086aed
Ayan Ginet <ayan.ginet@nu.edu.kz>
2020-12-29 13:44:27 +0500

    refactoring

--- tests/forms/test_address_upload.py
+++ tests/forms/test_address_upload.py
@@ -8,34 +8,32 @@ from pretix_eth.models import WalletAddress
 
 
 @pytest.fixture
-def env(event, organizer):
+def env(event):
     user = User.objects.create_user('admin@pretix', 'admin')
-    t = Team.objects.create(organizer=event.organizer,
-                            can_view_orders=True, can_change_orders=True)
-    t.members.add(user)
-    t.limit_events.add(event)
+    team = Team.objects.create(organizer=event.organizer,
+                               can_view_orders=True, can_change_orders=True)
+    team.members.add(user)
+    team.limit_events.add(event)
 
-    wallet_address = WalletAddress(
-        hex_address='0x604Bcfb802b11866222825967aCD7Ec44d44168B', event=event)
-    wallet_address.save()
-
-    return organizer, event
+    address = '0x604Bcfb802b11866222825967aCD7Ec44d44168B'
+    WalletAddress.objects.create(hex_address=address, event=event)
+    return address
 
 
 @pytest.mark.django_db
-def test_file_upload_success(client, env):
+def test_file_upload_success(client, organizer, event, env):
+    existing_wallet_address = env
+    assert WalletAddress.objects.all().count() == 1
+    assert WalletAddress.objects.filter(hex_address=existing_wallet_address).exists()
+
     client.login(email='admin@pretix', password='admin')
-
-    organizer, event = env
-
     request_url = f'/control/event/{organizer.slug}/{event.slug}/wallet-address-upload/'
-
     response = client.get(request_url)
 
     assert response.status_code == 200
     assert b'Wallet address upload' in response.content
 
-    file = SimpleUploadedFile('upload.txt', """
+    file = SimpleUploadedFile('upload.txt', f"""
         # OK
         0x38A670EB58F937D63D79D92dC651155595e66009
         # OK
@@ -45,7 +43,7 @@ def test_file_upload_success(client, env):
         # Commented
         # 0x0a648dD253CfBf0A7340C93fe7D8774FEfe972eD
         # Already exists in database
-        0x604Bcfb802b11866222825967aCD7Ec44d44168B
+        {existing_wallet_address}
         # Weird spacing
             0x06E5c4854D7A21A4C466bcA74F6A2867a8ef0fd2
     """.encode("utf-8"), content_type="text/plain")
@@ -63,43 +61,65 @@ def test_file_upload_success(client, env):
     assert response.status_code == 200
     assert b'Created 3 new wallet addresses!' in response.content
 
+    loaded_addresses = [
+        '0x38A670EB58F937D63D79D92dC651155595e66009',
+        '0x26AA4d587584FE1f32707070768236eDC625dDD7',
+        '0x06E5c4854D7A21A4C466bcA74F6A2867a8ef0fd2'
+    ]
+
+    for address in loaded_addresses:
+        assert WalletAddress.objects.filter(hex_address=address).exists()
+
 
 @pytest.mark.django_db
-def test_file_upload_error(client, env):
+def test_file_upload_length_short(client, organizer, event, env):
     client.login(email='admin@pretix', password='admin')
 
-    organizer, event = env
-
     request_url = f'/control/event/{organizer.slug}/{event.slug}/wallet-address-upload/'
-
-    invalid_file_1 = SimpleUploadedFile('upload.txt', """
-        # Length is too short
+    invalid_file = SimpleUploadedFile('upload.txt', """
         0x38A670EB58F9D63D79D92dC651155595e66009
     """.encode("utf-8"), content_type="text/plain")
 
-    response = client.post(request_url, {'wallet_addresses': invalid_file_1}, follow=True)
+    response = client.post(request_url, {'wallet_addresses': invalid_file}, follow=True)
     assert response.status_code == 200
     assert b'Syntax error on line 1:' in response.content
 
-    invalid_file_2 = SimpleUploadedFile('upload.txt', """
-        # Length is too long
+
+@pytest.mark.django_db
+def test_file_upload_length_long(client, organizer, event, env):
+    client.login(email='admin@pretix', password='admin')
+
+    request_url = f'/control/event/{organizer.slug}/{event.slug}/wallet-address-upload/'
+    invalid_file = SimpleUploadedFile('upload.txt', """
         0x38A670EB58F9D63Dfds32D279D92dC651155595e66009
     """.encode("utf-8"), content_type="text/plain")
 
-    response = client.post(request_url, {'wallet_addresses': invalid_file_2}, follow=True)
+    response = client.post(request_url, {'wallet_addresses': invalid_file}, follow=True)
     assert response.status_code == 200
     assert b'Syntax error on line 1:' in response.content
 
-    invalid_file_3 = SimpleUploadedFile('upload.txt', """
-        # Two addresses on the same line
+
+@pytest.mark.django_db
+def test_file_upload_one_address_per_line(client, organizer, event, env):
+    client.login(email='admin@pretix', password='admin')
+
+    request_url = f'/control/event/{organizer.slug}/{event.slug}/wallet-address-upload/'
+    invalid_file = SimpleUploadedFile('upload.txt', """
+        # Two addresses in the same line, expected one
         0xe61407b0708CC10006aAc0ceA62F553Ed84D2aD8 0xcD9989Dc8F0f02866eC945cBCCDd6CFA32D46026
     """.encode("utf-8"), content_type="text/plain")
 
-    response = client.post(request_url, {'wallet_addresses': invalid_file_3}, follow=True)
+    response = client.post(request_url, {'wallet_addresses': invalid_file}, follow=True)
     assert response.status_code == 200
     assert b'Syntax error on line 1:' in response.content
 
-    invalid_file_4 = SimpleUploadedFile('upload.txt', """
+
+@pytest.mark.django_db
+def test_file_upload_invalid_middle(client, organizer, event, env):
+    client.login(email='admin@pretix', password='admin')
+
+    request_url = f'/control/event/{organizer.slug}/{event.slug}/wallet-address-upload/'
+    invalid_file = SimpleUploadedFile('upload.txt', """
         # Invalid address in the middle of the file
         0xe61407b0708CC10006aAc0ceA62F553Ed84D2aD8
         0x378c3Ba28099187788500cb4a833FbA161F9879C
@@ -107,24 +127,42 @@ def test_file_upload_error(client, env):
         0x1F59154Bcf62A3B63Db28F306996540c0a6aD8a5
     """.encode("utf-8"), content_type="text/plain")
 
-    response = client.post(request_url, {'wallet_addresses': invalid_file_4}, follow=True)
+    response = client.post(request_url, {'wallet_addresses': invalid_file}, follow=True)
     assert response.status_code == 200
     assert b'Syntax error on line 3:' in response.content
 
-    invalid_file_5 = SimpleUploadedFile('upload.txt', """
+
+@pytest.mark.django_db
+def test_file_upload_invalid_prefix(client, organizer, event, env):
+    client.login(email='admin@pretix', password='admin')
+
+    request_url = f'/control/event/{organizer.slug}/{event.slug}/wallet-address-upload/'
+    invalid_file = SimpleUploadedFile('upload.txt', """
         # Prefix is not 0x
         e61407b0708CC10006aAc0ceA62F553Ed84D2aD8
     """.encode("utf-8"), content_type="text/plain")
 
-    response = client.post(request_url, {'wallet_addresses': invalid_file_5}, follow=True)
+    response = client.post(request_url, {'wallet_addresses': invalid_file}, follow=True)
     assert response.status_code == 200
     assert b'Syntax error on line 1:' in response.content
 
+
+@pytest.mark.django_db
+def test_file_upload_empty_file(client, organizer, event, env):
+    client.login(email='admin@pretix', password='admin')
+
+    request_url = f'/control/event/{organizer.slug}/{event.slug}/wallet-address-upload/'
     empty_file = SimpleUploadedFile('upload.txt', ''.encode("utf-8"), content_type="text/plain")
     response = client.post(request_url, {'wallet_addresses': empty_file}, follow=True)
     assert response.status_code == 200
     assert b'The submitted file is empty.' in response.content
 
+
+@pytest.mark.django_db
+def test_file_upload_no_addresses_in_file(client, organizer, event, env):
+    client.login(email='admin@pretix', password='admin')
+
+    request_url = f'/control/event/{organizer.slug}/{event.slug}/wallet-address-upload/'
     no_address_file = SimpleUploadedFile(
         'upload.txt', '#'.encode("utf-8"), content_type="text/plain")
     response = client.post(request_url, {'wallet_addresses': no_address_file}, follow=True)
