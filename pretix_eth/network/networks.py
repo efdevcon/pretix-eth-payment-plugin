9c8f99d9267c279ead26b4842daa3183e2fe509c
Rahul Kothari <rahul.kothari.201@gmail.com>
2021-06-29 15:11:31 +0100

    use class variables

--- pretix_eth/network/networks.py
+++ pretix_eth/network/networks.py
@@ -58,25 +58,27 @@ class INetwork(object):
 
 class Rinkeby(INetwork):
     """ Implementation for Rinkeby Testnet """
+
+    CHAIN_ID = 4
+    DAI_ADDRESS = '0xc7AD46e0b8a400Bb3C915120d284AafbA8fc4735'
+
     def __init__(self):
         super(Rinkeby, self).__init__("Rinkeby")
-        self.CHAIN_ID = 4
-        self.DAI_ADDRESS = '0xc7AD46e0b8a400Bb3C915120d284AafbA8fc4735'
 
     def payment_instructions(self, wallet_address, payment_amount, amount_in_ether_or_dai, currency_type):
         """
         Instructions for paying ETH or DAI in L1 Ethereum. Pay via a web3 modal, ERC 681 (QR Code), uniswap url or manually.
         """
         if currency_type == 'ETH':
-            erc_681_url = make_erc_681_url(wallet_address, payment_amount, self.CHAIN_ID)
+            erc_681_url = make_erc_681_url(wallet_address, payment_amount, Rinkeby.CHAIN_ID)
             amount_manual = f'{amount_in_ether_or_dai} ETH'
             uniswap_url = make_uniswap_url(
-                ETH, wallet_address, amount_in_ether_or_dai, self.DAI_ADDRESS)
+                ETH, wallet_address, amount_in_ether_or_dai, Rinkeby.DAI_ADDRESS)
         elif currency_type == 'DAI':
-            erc_681_url = make_erc_681_url(wallet_address, payment_amount, self.CHAIN_ID, is_token=True, token_address=self.DAI_ADDRESS)
+            erc_681_url = make_erc_681_url(wallet_address, payment_amount, Rinkeby.CHAIN_ID, is_token=True, token_address=Rinkeby.DAI_ADDRESS)
             amount_manual = f'{amount_in_ether_or_dai} DAI'
             uniswap_url = make_uniswap_url(
-                self.DAI_ADDRESS, wallet_address, amount_in_ether_or_dai)
+                Rinkeby.DAI_ADDRESS, wallet_address, amount_in_ether_or_dai)
         else:
             raise ImproperlyConfigured(f'Unrecognized currency: {currency_type}')  # noqa: E501
        
@@ -93,9 +95,11 @@ class Rinkeby(INetwork):
 
 class L1(INetwork):
     """ Implementation for Ethereum L1 """
+    
+    DAI_ADDRESS = '0x6b175474e89094c44da98b954eedeac495271d0f'
+    
     def __init__(self):
         super(L1, self).__init__("L1")
-        self.DAI_ADDRESS = '0x6b175474e89094c44da98b954eedeac495271d0f'
 
     def payment_instructions(self, wallet_address, payment_amount, amount_in_ether_or_dai, currency_type): # noqa: E501
         """
@@ -105,13 +109,13 @@ class L1(INetwork):
             erc_681_url = make_erc_681_url(wallet_address, payment_amount)
             amount_manual = f'{amount_in_ether_or_dai} ETH'
             uniswap_url = make_uniswap_url(
-                ETH, wallet_address, amount_in_ether_or_dai, self.DAI_ADDRESS)
+                ETH, wallet_address, amount_in_ether_or_dai, L1.DAI_ADDRESS)
         elif currency_type == 'DAI':
             erc_681_url = make_erc_681_url(
-                wallet_address, payment_amount, is_token=True, token_address=self.DAI_ADDRESS) 
+                wallet_address, payment_amount, is_token=True, token_address=L1.DAI_ADDRESS) 
             amount_manual = f'{amount_in_ether_or_dai} DAI'
             uniswap_url = make_uniswap_url(
-                self.DAI_ADDRESS, wallet_address, amount_in_ether_or_dai)
+                L1.DAI_ADDRESS, wallet_address, amount_in_ether_or_dai)
 
         else:
             raise ImproperlyConfigured(f'Unrecognized currency: {currency_type}') 
