e212cf7ad40c6597965d1ea3813ae2fadc3f05b8
Rahul Kothari <rahul.kothari.201@gmail.com>
2021-07-12 22:49:33 +0100

    refactor confirm-refunds, lint, fix imports

--- pretix_eth/management/commands/confirm_refunds.py
+++ pretix_eth/management/commands/confirm_refunds.py
@@ -4,68 +4,44 @@ from django.core.management.base import (
     BaseCommand,
 )
 from django_scopes import scope
+
 from pretix.base.models.event import (
     Event,
 )
-from web3 import (
-    Web3,
-)
-from web3.providers.auto import (
-    load_provider_from_uri,
+from pretix.base.models import (
+    OrderRefund,
 )
 
 from pretix_eth.models import (
     WalletAddress,
 )
-
-from pretix.base.models import (
-    OrderRefund,
-)
+from pretix_eth.network.networks import all_network_ids_to_networks
 
 logger = logging.getLogger(__name__)
-
-TOKEN_ABI = [
-    {'constant': True,
-     'inputs': [{'name': '_owner', 'type': 'address'}],
-     'name': 'balanceOf',
-     'outputs': [{'name': 'balance', 'type': 'uint256'}],
-     'type': 'function'},
-]
 
 
 class Command(BaseCommand):
     help = (
-        'Verify pending refunds from on-chain payments.  Performs a dry run '
-        'by default.'
+        "Verify pending refunds from on-chain payments.  Performs a dry run "
+        "by default."
     )
 
     def add_arguments(self, parser):
         parser.add_argument(
-            '-s', '--event-slug',
-            help='The slug of the event for which refunds should be confirmed.',
+            "-s",
+            "--event-slug",
+            help="The slug of the event for which refunds should be confirmed.",
         )
         parser.add_argument(
-            '-n', '--no-dry-run',
-            help='Modify database records to confirm refunds.',
-            action='store_true',
-        )
-        parser.add_argument(
-            '-u', '--web3-provider-uri',
-            help='A provider uri used to initialize web3.',
-        )
-        parser.add_argument(
-            '-a', '--token-address',
-            help='The token address used to check for token payments.',
+            "-n",
+            "--no-dry-run",
+            help="Modify database records to confirm refunds.",
+            action="store_true",
         )
 
     def handle(self, *args, **options):
-        slug = options['event_slug']
-        no_dry_run = options['no_dry_run']
-        uri = options['web3_provider_uri']
-        token_address = options['token_address']
-
-        w3 = Web3(load_provider_from_uri(uri))
-        token_contract = w3.eth.contract(abi=TOKEN_ABI, address=token_address)
+        slug = options["event_slug"]
+        no_dry_run = options["no_dry_run"]
 
         try:
             with scope(organizer=None):
@@ -77,26 +53,42 @@ class Command(BaseCommand):
 
         for wallet_address in unconfirmed_addresses:
             hex_address = wallet_address.hex_address
-            checksum_address = w3.toChecksumAddress(hex_address)
 
             order_refund = OrderRefund.objects.get(payment=wallet_address.order_payment)
             full_id = order_refund.full_id
 
             info = order_refund.info_data
-            expected_currency_type = info['currency_type']
+            currency_info = info["currency_type"].split("-")
+            expected_currency_type = currency_info[0]
+            expected_network_id = currency_info[1]
+            expected_network_rpc_url_key = f"{expected_network_id}_RPC_URL"
+            network_rpc_url = None
 
-            eth_amount = w3.eth.getBalance(checksum_address)
-            token_amount = token_contract.functions.balanceOf(checksum_address).call()
+            if expected_network_rpc_url_key in rpc_urls:
+                network_rpc_url = rpc_urls[expected_network_rpc_url_key]
+            else:
+                # TODO: Give an option for caller to add rpc url at run time.
+                logger.error(f"RPC URL not configured for {expected_network_id}")
+                continue
 
-            if (eth_amount == 0 and expected_currency_type == 'ETH') or (
-                    token_amount == 0 and expected_currency_type == 'DAI'):
-                logger.info(f'Refund found for {full_id} at {checksum_address}:')
+            expected_network = all_network_ids_to_networks[expected_network_id]
+            expected_amount = info["amount"]
+
+            # Get eth, token balance.
+            eth_amount, token_amount = expected_network.get_currency_balance(
+                hex_address, network_rpc_url
+            )
+
+            if (eth_amount == 0 and expected_currency_type == "ETH") or (
+                token_amount == 0 and expected_currency_type == "DAI"
+            ):
+                logger.info(f"Refund found for {full_id} at {hex_address}:")
 
                 if no_dry_run:
-                    logger.info(f'  * Confirming refund payment {full_id}')
+                    logger.info(f"  * Confirming refund payment {full_id}")
                     with scope(organizer=None):
                         order_refund.done()
                 else:
-                    logger.info(f'  * DRY RUN: Would confirm order payment {full_id}')
+                    logger.info(f"  * DRY RUN: Would confirm order payment {full_id}")
             else:
-                logger.info(f'No refund process started for {full_id}')
+                logger.info(f"No refund process started for {full_id}")
