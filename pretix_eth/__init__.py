83be7bc84922791c4a9b1e05816f5def624a0851
ligi <ligi@ligi.de>
2021-10-05 21:38:16 +0200

    Bump version to 2.0.5-dev

--- pretix_eth/__init__.py
+++ pretix_eth/__init__.py
@@ -12,7 +12,7 @@ class PluginApp(AppConfig):
         category = 'PAYMENT'
         description = ugettext_lazy('An ethereum payment provider plugin for pretix software')
         visible = True
-        version = '2.0.4-dev'
+        version = '2.0.5-dev'
 
     def ready(self):
         from . import signals  # NOQA
