f6d157922c932c45b434521c9b4a6674b69cfc30
mikulas-mrva <mikulas.mrva@gmail.com>
2022-07-08 01:20:44 +0200

    Improving user flow of the payment process, displaying a message when the user should sign a message and confirm a transaction

--- pretix_eth/static/pretix_eth/web3modal.js
+++ pretix_eth/static/pretix_eth/web3modal.js
@@ -2,37 +2,43 @@
 
 var selectedAccount = '';  // address of the currently connected account
 var signedByAccount = '';  // address of the account that has signed the message, to check on account chages
-var hasSigned = false;  // true if user has sent a message
+var signature = false;  // true if user has sent a message
 var hasPaid = false;  // true if user has signed the transaction
-
+var paymentDetails;
 // todo display errors!
 // todo pay with DAI also!
+// todo display instructions:
+//   1. connect wallet
+//   2. check netwwork
+//   3. sign a message
+//   4. send payment
+//   5. do we need a disconnect button?
 
 function getCookie(name) {
-  // Add the = sign
-  name = name + '=';
+    // Add the = sign
+    name = name + '=';
 
-  // Get the decoded cookie
-  const decodedCookie = decodeURIComponent(document.cookie);
+    // Get the decoded cookie
+    const decodedCookie = decodeURIComponent(document.cookie);
 
-  // Get all cookies, split on ; sign
-  const cookies = decodedCookie.split(';');
+    // Get all cookies, split on ; sign
+    const cookies = decodedCookie.split(';');
 
-  // Loop over the cookies
-  for (let i = 0; i < cookies.length; i++) {
-    // Define the single cookie, and remove whitespace
-    const cookie = cookies[i].trim();
+    // Loop over the cookies
+    for (let i = 0; i < cookies.length; i++) {
+        // Define the single cookie, and remove whitespace
+        const cookie = cookies[i].trim();
 
-    // If this cookie has the name of what we are searching
-    if (cookie.indexOf(name) == 0) {
-      // Return everything after the cookies name
-      return cookie.substring(name.length, cookie.length);
+        // If this cookie has the name of what we are searching
+        if (cookie.indexOf(name) == 0) {
+            // Return everything after the cookies name
+            return cookie.substring(name.length, cookie.length);
+        }
     }
-  }
 }
 
-async function getPaymentTransactionData(walletAddress){
-    // todo should say if we're waiting for another payment from that address
+async function getPaymentTransactionData(){
+    let walletAddress = await getAccount();
     const url = document.getElementById("btn-connect").getAttribute("data-transaction-details-url")
     const response = await fetch(url + '?' + new URLSearchParams({
         sender_address: walletAddress
@@ -53,20 +59,19 @@ async function submitSignature(signature, transactionHash, selectedAccount) {
         csrfmiddlewaretoken: csrf_cookie
     })
     fetch(url, {
-      headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-          'X-CSRF-TOKEN': csrf_cookie,
-          'HTTP-X-CSRFTOKEN': csrf_cookie,
-      },
-      method: 'POST',
-      body: searchParams
-    }
-  )
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+                'X-CSRF-TOKEN': csrf_cookie,
+                'HTTP-X-CSRFTOKEN': csrf_cookie,
+            },
+            method: 'POST',
+            body: searchParams
+        }
+    )
 }
 
 function init() {
     document.querySelector("#prepare").style.display = "block";
-    document.querySelector("#connected").style.display = "none";
 
     const providerOptions = {
         walletconnect: {
@@ -86,159 +91,189 @@ function showError(message) {
     document.querySelector("#message-error").innerHTML = message;
 }
 
-async function submitTransaction(paymentDetails, signature) {
-  var transactionHash;
-  // make payment
-  if (paymentDetails['erc20_contract_address'] !== null) { // erc20 transfer
-    const contract = new Contract(
-      asset.contractAddress,
-      ERC20.abi,
-      ethersProvider.getSigner()
+function displayOnlyId(divId) {
+    document.querySelectorAll(".pretix-eth-payment-steps").forEach(
+        function(div) {
+            if (div.id === divId) {
+                div.style.display = "block";
+            } else {
+                div.style.display = "none";
+            }
+        }
     );
-    const tx = await contract.transfer(
-      to,
-      utils.parseUnits(amount, BigNumber.from(asset.decimals))
-    );
-    transactionHash = tx.hash;
-    submitSignature(signature, transactionHash, selectedAccount);
-  } else { // crypto transfer
-    await window.web3.eth.sendTransaction(
-      {
-        from: selectedAccount,
-        to: paymentDetails['recipient_address'],
-        value: paymentDetails['amount'],
-      }
-      ).on(
-        'transactionHash',
-          function (transactionHash) {
+}
+
+async function submitTransaction() {
+    // todo move signature to a global var
+    async function _submitTransaction() {
+        displayOnlyId("send-transaction")
+        var selectedAccount = await getAccount();
+        var transactionHash;
+        // make payment
+        if (paymentDetails['erc20_contract_address'] !== null) { // erc20 transfer
+            // TODO !!
+            const contract = new Contract(
+                paymentDetails['erc20_contract_address'],
+                ERC20.abi,  // todo
+                ethersProvider.getSigner()
+            );
+            const tx = await contract.transfer(
+                to,
+                utils.parseUnits(amount, BigNumber.from(asset.decimals))
+            );
+            transactionHash = tx.hash;
             submitSignature(signature, transactionHash, selectedAccount);
-          }
-        )
-      }
-  }
+        } else { // crypto transfer
+            await window.web3.eth.sendTransaction(
+                {
+                    from: selectedAccount,
+                    to: paymentDetails['recipient_address'],
+                    value: paymentDetails['amount'],
+                }
+            ).on(
+                'transactionHash',
+                function (transactionHash) {
+                    submitSignature(signature, transactionHash, selectedAccount);
+                }
+            )
+        }
+    }
+    try {
+        await _submitTransaction(signature);
+    } catch (error) {
+        showError(error);
+    }
+}
+
+async function getAccount() {
+    // Get a Web3 instance for the wallet
+    const accounts = await window.web3.eth.getAccounts();
+    // MetaMask does not give you all accounts, only the selected account
+    return accounts[0];
+}
 
 /*
 * Called on "Connect wallet and pay" button click and every chain/account change
 */
 async function makePayment() {
-
-  async function _makePayment() {
-    /* todo wrap this in button disable and un-disable and a try-except block  */
-
-    // Get a Web3 instance for the wallet
-      const accounts = await window.web3.eth.getAccounts();
-    // MetaMask does not give you all accounts, only the selected account
-    selectedAccount = accounts[0];
-
-    const paymentDetails = await getPaymentTransactionData(selectedAccount);
-    if (paymentDetails['is_signature_submitted'] === true) {
-      showError("It seems that you have paid for this order already.")
-      return
-    }
-
-    // todo check that payment can be made form this wallet
-
-    // Make sure we're connected to the right chain
-    const currentChainId = await window.web3.eth.getChainId()
-    if (paymentDetails['chain_id'] !== currentChainId) {
-      let desiredChainId = '0x'+paymentDetails['chain_id'].toString(16);
-      window.ethereum.request(
-        {
-          method: 'wallet_switchEthereumChain',
-          params: [{ chainId: desiredChainId}]
+    async function _checkChainAndProceed() {
+        const selectedAccount = await getAccount();
+        paymentDetails = await getPaymentTransactionData(selectedAccount);
+        if (paymentDetails['is_signature_submitted'] === true) {
+            showError("It seems that you have paid for this order already.")
+            return
         }
-      )
-    }
-
-    // sign the message
-    if (!hasSigned || selectedAccount !== signedByAccount) {
-      let message = JSON.stringify(paymentDetails['message']);
-      window.web3.currentProvider.sendAsync(
-        {
-            method: "eth_signTypedData_v3",
-            params: [selectedAccount, message],
-            from: selectedAccount
-        },
-        async function(err, result) {
-          if (err) {
-            debugger;
-            return console.log(err);
-          }
-          const signature = result.result;
-          await submitTransaction(paymentDetails, signature)
+        if (paymentDetails['has_other_unpaid_orders'] === true) {
+            showError("Please wait for other payments from your wallet to be confirmed before submitting another transaction.")
+            return
         }
-      );
-      hasSigned = true;
-      signedByAccount = selectedAccount;
+
+        // Make sure we're connected to the right chain
+        const currentChainId = await window.web3.eth.getChainId()
+        if (paymentDetails['chain_id'] !== currentChainId) {
+            // todo set-correct-network
+            // Subscribe to chainId change
+            provider.on("chainChanged", (chainId) => {
+                signMessage();
+            });
+
+            // Subscribe to networkId change
+            provider.on("networkChanged", (networkId) => {
+                signMessage();
+            });
+
+            let desiredChainId = '0x'+paymentDetails['chain_id'].toString(16);
+            window.ethereum.request(
+                {
+                    method: 'wallet_switchEthereumChain',
+                    params: [{ chainId: desiredChainId}]
+                }
+            )
+        } else {
+            await signMessage();
+        }
     }
-  }
-  try {
-    await _makePayment();
-  } catch (error) {
-    console.error(error); // tslint:disable-line
-    showError(error);
-  }
+    // refresh paymentDetails in case account has changed
+    paymentDetails = await getPaymentTransactionData();
+    try {
+        await _checkChainAndProceed();
+    } catch (error) {
+        showError(error);
+    }
+}
+
+/* */
+async function signMessage() {
+    async function _signMessage() {
+        displayOnlyId("sign-a-message");
+
+        let selectedAccount = await getAccount();
+        // sign the message
+        if (!signature || selectedAccount !== signedByAccount) {
+            let message = JSON.stringify(paymentDetails['message']);
+            window.web3.currentProvider.sendAsync(
+                {
+                    method: "eth_signTypedData_v3",
+                    params: [selectedAccount, message],
+                    from: selectedAccount
+                },
+                async function (err, result) {
+                    if (err) {
+                        showError(err)
+                        return console.log(err);
+                    }
+                    signature = result.result;
+                    signedByAccount = selectedAccount;
+                    await submitTransaction()
+                }
+            );
+        }
+    }
+    const button = document.querySelector("#retry-sign-a-message")
+    button.setAttribute("disabled", "disabled")
+    try {
+        _signMessage();
+    } catch (error) {
+        button.removeAttribute("disabled")
+        showError(error);
+    }
 }
 
 /**
  * Connect wallet button pressed.
  */
 async function web3ModalOnConnect() {
-  let provider;
-  try {
-    provider = await web3Modal.connect();
-  } catch(e) {
-    console.log("Could not get a wallet connection", e);
-    return;
-  }
+    let provider;
+    try {
+        provider = await web3Modal.connect();
+    } catch(e) {
+        console.log("Could not get a wallet connection", e);
+        return;
+    }
 
-  // Subscribe to accounts change
-  provider.on("accountsChanged", (accounts) => {
-    makePayment();
-  });
+    // Subscribe to accounts change
+    provider.on("accountsChanged", (accounts) => {
+        makePayment();
+    });
 
-  // Subscribe to chainId change
-  provider.on("chainChanged", (chainId) => {
-    makePayment();
-  });
+    // Subscribe to chainId change
+    provider.on("chainChanged", (chainId) => {
+        makePayment();
+    });
 
-  // Subscribe to networkId change
-  provider.on("networkChanged", (networkId) => {
-    makePayment();
-  });
+    // Subscribe to networkId change
+    provider.on("networkChanged", (networkId) => {
+        makePayment();
+    });
 
-  window.web3 = new Web3(provider);
+    window.web3 = new Web3(provider);
 
-  document.querySelector("#btn-connect").setAttribute("disabled", "disabled");
-  await makePayment(provider);
-  document.querySelector("#btn-connect").removeAttribute("disabled");
-}
-
-/**
- * Disconnect wallet button pressed.
- */
-async function web3ModalOnDisconnect() {
-
-  if(provider.close) {
-    await provider.close();
-
-    // If the cached provider is not cleared,
-    // WalletConnect will default to the existing session
-    // and does not allow to re-scan the QR code with a new wallet.
-    // Depending on your use case you may want or want not his behavir.
-    await web3Modal.clearCachedProvider();
-    provider = null;
-  }
-
-  selectedAccount = null;
-
-  // Set the UI back to the initial state
-  document.querySelector("#prepare").style.display = "block";
-  document.querySelector("#connected").style.display = "none";
+    document.querySelector("#btn-connect").setAttribute("disabled", "disabled");
+    await makePayment(provider);
+    document.querySelector("#btn-connect").removeAttribute("disabled");
 }
 
 window.addEventListener('load', async () => {
-  init();
-  document.querySelector("#btn-connect").addEventListener("click", web3ModalOnConnect);
-  document.querySelector("#btn-disconnect").addEventListener("click", web3ModalOnDisconnect);
+    init();
+    document.querySelector("#btn-connect").addEventListener("click", web3ModalOnConnect);
 });
