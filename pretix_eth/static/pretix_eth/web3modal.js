eefabbbe6b2fd111e7e3ae1e35c2e12280941f1c
mikulas-mrva <mikulas.mrva@gmail.com>
2022-07-14 00:47:54 +0200

    Handling user dialogue cancellations gracefully.

--- pretix_eth/static/pretix_eth/web3modal.js
+++ pretix_eth/static/pretix_eth/web3modal.js
@@ -3,16 +3,8 @@
 var selectedAccount = '';  // address of the currently connected account
 var signedByAccount = '';  // address of the account that has signed the message, to check on account chages
 var signature = false;  // true if user has sent a message
-var hasPaid = false;  // true if user has signed the transaction
 var paymentDetails;
-// todo display errors!
 // todo pay with DAI also!
-// todo display instructions:
-//   1. connect wallet
-//   2. check netwwork
-//   3. sign a message
-//   4. send payment
-//   5. do we need a disconnect button?
 
 function getCookie(name) {
     // Add the = sign
@@ -50,32 +42,39 @@ async function getPaymentTransactionData(){
 }
 
 async function submitSignature(signature, transactionHash, selectedAccount) {
-    let csrf_cookie = getCookie('pretix_csrftoken')
-    const url = document.getElementById("btn-connect").getAttribute("data-transaction-details-url")
-    let searchParams = new URLSearchParams({
-        signedMessage: signature,
-        transactionHash: transactionHash,
-        selectedAccount: selectedAccount,
-        csrfmiddlewaretoken: csrf_cookie
-    })
-    fetch(url, {
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded",
-                'X-CSRF-TOKEN': csrf_cookie,
-                'HTTP-X-CSRFTOKEN': csrf_cookie,
-            },
-            method: 'POST',
-            body: searchParams
-        }
-    ).then((response) => {
-            if (response.ok) {
-                document.getElementById("pretix-eth-transaction-hash").innerText = transactionHash;
-                displayOnlyId("success");
-            } else {
-                showError("There was an error processing your payment, please contact support. Your payment was sent in transaction "+transactionHash+".")
+    async function _submitSignature(signature, transactionHash, selectedAccount) {
+        let csrf_cookie = getCookie('pretix_csrftoken')
+        const url = document.getElementById("btn-connect").getAttribute("data-transaction-details-url")
+        let searchParams = new URLSearchParams({
+            signedMessage: signature,
+            transactionHash: transactionHash,
+            selectedAccount: selectedAccount,
+            csrfmiddlewaretoken: csrf_cookie
+        })
+        fetch(url, {
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded",
+                    'X-CSRF-TOKEN': csrf_cookie,
+                    'HTTP-X-CSRFTOKEN': csrf_cookie,
+                },
+                method: 'POST',
+                body: searchParams
             }
-        }
-    )
+        ).then((response) => {
+                if (response.ok) {
+                    document.getElementById("pretix-eth-transaction-hash").innerText = transactionHash;
+                    displayOnlyId("success");
+                } else {
+                    showError("There was an error processing your payment, please contact support. Your payment was sent in transaction " + transactionHash + ".", false)
+                }
+            }
+        )
+    }
+    try {
+        await _submitSignature(signature, transactionHash, selectedAccount);
+    } catch (error) {
+        showError(error, true);
+    }
 }
 
 function init() {
@@ -95,9 +94,28 @@ function init() {
     });
 }
 
-function showError(message) {
-    document.querySelector("#message-error").innerHTML = message;
+function showError(message = '', reset_state = true) {
+    if (typeof message === "object") {
+        if (message.message !== undefined) {
+            message = message.message + " Please try again."
+        }
+    }
+
+    if (message === "") {
+        message = "There was an error, please try again, or contact support if you have already confirmed a payment in your wallet provider."
+    }
+    document.getElementById("message-error").innerHTML = message;
+    if (reset_state === true) {
+        displayOnlyId("prepare");
+    }
+    return false
 }
+
+function resetErrorMessage() {
+    document.getElementById("message-error").innerHTML = '';
+}
+
+
 
 function displayOnlyId(divId) {
     document.querySelectorAll(".pretix-eth-payment-steps").forEach(
@@ -112,13 +130,14 @@ function displayOnlyId(divId) {
 }
 
 async function submitTransaction() {
-    // todo move signature to a global var
     async function _submitTransaction() {
         displayOnlyId("send-transaction")
         var selectedAccount = await getAccount();
+        let paymentDetails = await getPaymentTransactionData();
         var transactionHash;
-        // make payment
-        if (paymentDetails['erc20_contract_address'] !== null) { // erc20 transfer
+        // make the payment
+        if (paymentDetails['erc20_contract_address'] !== null) {
+            // erc20 transfer
             // TODO !!
             const contract = new Contract(
                 paymentDetails['erc20_contract_address'],
@@ -143,17 +162,24 @@ async function submitTransaction() {
                 function (transactionHash) {
                     submitSignature(signature, transactionHash, selectedAccount);
                 }
-            )
+            ).on(
+                'error', showError
+            ).catch(
+                showError
+            );
         }
     }
     try {
         await _submitTransaction(signature);
     } catch (error) {
-        showError(error);
+        showError(error, true);
     }
 }
 
 async function getAccount() {
+    if (window.web3 === undefined || window.web3.eth === undefined) {
+        await initWeb3();
+    }
     // Get a Web3 instance for the wallet
     const accounts = await window.web3.eth.getAccounts();
     // MetaMask does not give you all accounts, only the selected account
@@ -165,8 +191,8 @@ async function getAccount() {
 */
 async function makePayment() {
     async function _checkChainAndProceed() {
-        const selectedAccount = await getAccount();
-        paymentDetails = await getPaymentTransactionData(selectedAccount);
+        // refresh paymentDetails in case account has changed
+        let paymentDetails = await getPaymentTransactionData();
         if (paymentDetails['is_signature_submitted'] === true) {
             showError("It seems that you have paid for this order already.")
             return
@@ -179,9 +205,8 @@ async function makePayment() {
         // Make sure we're connected to the right chain
         const currentChainId = await window.web3.eth.getChainId()
         if (paymentDetails['chain_id'] !== currentChainId) {
-            // todo set-correct-network
             // Subscribe to chainId change
-            let provider = await web3Modal.connect();
+            let provider = await initWeb3();
             provider.on("chainChanged", (chainId) => {
                 signMessage();
             });
@@ -202,12 +227,11 @@ async function makePayment() {
             await signMessage();
         }
     }
-    // refresh paymentDetails in case account has changed
-    paymentDetails = await getPaymentTransactionData();
+    resetErrorMessage();
     try {
         await _checkChainAndProceed();
     } catch (error) {
-        showError(error);
+        showError(error, true);
     }
 }
 
@@ -215,10 +239,13 @@ async function makePayment() {
 async function signMessage() {
     async function _signMessage() {
         displayOnlyId("sign-a-message");
-
         let selectedAccount = await getAccount();
+        let paymentDetails = await getPaymentTransactionData();
         // sign the message
-        if (!signature || selectedAccount !== signedByAccount) {
+        if (signature && selectedAccount === signedByAccount) {
+            // skip the signature step if we have one already
+            await submitTransaction();
+        } else {
             let message = JSON.stringify(paymentDetails['message']);
             window.web3.currentProvider.sendAsync(
                 {
@@ -228,30 +255,25 @@ async function signMessage() {
                 },
                 async function (err, result) {
                     if (err) {
-                        showError(err)
-                        return console.log(err);
+                        showError(err, true)
+                    } else {
+                        signature = result.result;
+                        signedByAccount = selectedAccount;
+                        await submitTransaction();
                     }
-                    signature = result.result;
-                    signedByAccount = selectedAccount;
-                    await submitTransaction()
                 }
             );
         }
     }
-    const button = document.querySelector("#retry-sign-a-message")
-    button.setAttribute("disabled", "disabled")
     try {
         _signMessage();
     } catch (error) {
-        button.removeAttribute("disabled")
         showError(error);
     }
 }
 
-/**
- * Connect wallet button pressed.
- */
-async function web3ModalOnConnect() {
+
+async function initWeb3() {
     let provider;
     try {
         provider = await web3Modal.connect();
@@ -259,6 +281,7 @@ async function web3ModalOnConnect() {
         console.log("Could not get a wallet connection", e);
         return;
     }
+    window.web3 = new Web3(provider);
 
     // Subscribe to accounts change
     provider.on("accountsChanged", (accounts) => {
@@ -275,16 +298,38 @@ async function web3ModalOnConnect() {
         makePayment();
     });
 
-    window.web3 = new Web3(provider);
+    return provider
+}
 
+/**
+ * Connect wallet button pressed.
+ */
+async function web3ModalOnConnect() {
     document.getElementById("btn-connect").setAttribute("disabled", "disabled");
-    await makePayment(provider);
+    try {
+        await makePayment();
+    } catch (error) {
+        showError(error)
+    }
+
     document.getElementById("btn-connect").removeAttribute("disabled");
-    document.getElementById("retry-sign-a-message").onclick = signMessage();
-    document.getElementById("retry-send-transaction").onclick = submitTransaction();
 }
 
 window.addEventListener('load', async () => {
     init();
     document.querySelector("#btn-connect").addEventListener("click", web3ModalOnConnect);
 });
+
+window.onerror = function (message, file, line, col, error) {
+    showError(error.message)
+    return false;
+};
+
+window.addEventListener("error", function (e) {
+    showError(e.error.message)
+    return false;
+})
+
+window.addEventListener('unhandledrejection', function (e) {
+    showError()
+})
