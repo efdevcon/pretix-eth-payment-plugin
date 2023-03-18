aa158fe02e2ee36c19944186ed2d76f6b9f8666a
lassejaco <lasse.jacobsen@ethereum.org>
2023-03-19 00:02:12 +0900

    Revert to version 1 and upgrade packages again

--- pretix_eth/static/pretix_eth/web3modal/src/web3modal.js
+++ pretix_eth/static/pretix_eth/web3modal/src/web3modal.js
@@ -28,7 +28,7 @@ async function init() {
 
     const wagmiClient = createClient({
         autoConnect: true,
-        connectors: [...w3mConnectors({ projectId: walletConnectProjectId, version: 2, chains })],
+        connectors: w3mConnectors({ projectId: walletConnectProjectId, version: 1 /* Setting version 2 gives a range of issues https://github.com/WalletConnect/web3modal/issues/937 */, chains }),
         provider,
     });
 
