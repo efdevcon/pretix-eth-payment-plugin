0abd2e0280a38764d3dd013dbc2df91862c46236
lassejaco <lasse.jacobsen@ethereum.org>
2024-01-31 15:32:01 +0900

    fix coingecko and integration test

--- tests/integration/test_confirm_payments.py
+++ tests/integration/test_confirm_payments.py
@@ -3,12 +3,13 @@ import os
 from django.core.management import call_command
 import pytest
 import time
-
 from django.test import RequestFactory
-
+from pretix_eth.models import SignedMessage 
 
 WEB3_PROVIDER_URI = os.environ.get("WEB3_PROVIDER_URI")
-SINGLE_RECEIVER_ADDRESS = "0x0000000000000000000000000000000000000000"
+# WEB3_PROVIDER_URI = 'https://goerli.infura.io/v3/8733c260b59e445e85ee90bd80d869aa'
+# SINGLE_RECEIVER_ADDRESS = "0x0000000000000000000000000000000000000000"
+SINGLE_RECEIVER_ADDRESS = '0x47ABC45600bFb8069f53E55638Da593313e352C3'
 
 
 def check_web3_provider(pytesconfig):
@@ -40,7 +41,7 @@ def get_request_order_payment(get_order_and_payment):
         request.session = {
             "payment_currency_type": info_data["currency_type"],
             "payment_time": info_data["time"],
-            "payment_amount": info_data["amount"],
+            "payment_amount": info_data["amount"]
         }
         return request, order, payment
 
@@ -48,13 +49,15 @@ def get_request_order_payment(get_order_and_payment):
 
 
 def make_order_payment(payment_info, provider, get_request_order_payment):
-    order_kwargs = {"total": payment_info["amount"]}
-    payment_kwargs = {"amount": payment_info["amount"]}
+    order_kwargs = {"total": 0 } 
+    payment_kwargs = {"amount": 0 } 
+
     info_data = {
         "currency_type": payment_info["currency"],
         "time": int(time.time()),
-        "amount": payment_info["amount"],
+        "amount": payment_info["amount"]
     }
+
     request, order, payment = get_request_order_payment(
         order_kwargs=order_kwargs, payment_kwargs=payment_kwargs, info_data=info_data
     )
@@ -62,18 +65,20 @@ def make_order_payment(payment_info, provider, get_request_order_payment):
 
     return order, payment
 
-
 TEST_ENOUGH_AMOUNT = [
     {
         "currency": "ETH - Goerli",
-        "amount": int("1000", base=10),
-    },  # Has about 0.5 ETH
+        "amount": 40000000000000, # Required amount in wei
+        "signature": "0xf0c9df2bfb0c78754342a142cbafe58ff4cca3e0377c60d7596067abcbbd161c3641969f7135b29710408ab0a975d11e3ba0c609da0525a311937a6161db39461c",
+        "transaction_hash": '0x0cfd2f6f75bd37bf3286ac957964e1671560963b5abc004fe71fd74cd6cb6e45'
+    }, 
     {
         "currency": "DAI - Goerli",
-        "amount": int("1000", base=10),
-    },  # Has about 48 DAI
+        "amount": 100000000000000000, # 0.1 dai
+        "signature": "0x3345154d1069fe35e0396366ef9d6960974d9351b3cd46b1c31420ba6549bea0092395380e9140ec7d158343b08a93b3dd6c41130b10c65a3d420cca0cfc5ccd1c",
+        "transaction_hash": '0x196aaf0114721e1e94d661c903b2a7b957d25c936bf559d0eeca23d99914e3c7'
+    },
 ]
-
 
 @pytest.mark.django_db(
     transaction=True,
@@ -88,10 +93,23 @@ def test_confirm_payment_enough(
 
     payments = []
     orders = []
+
     for payment_info in TEST_ENOUGH_AMOUNT:
         order, payment = make_order_payment(
             payment_info, provider, get_request_order_payment
         )
+
+        # Add signed message to each order
+        message_obj = SignedMessage(
+            signature=payment_info['signature'],
+            sender_address=SINGLE_RECEIVER_ADDRESS,
+            recipient_address=SINGLE_RECEIVER_ADDRESS,
+            chain_id=5,
+            order_payment=payment,
+            transaction_hash=payment_info['transaction_hash'],
+        )
+
+        message_obj.save()
         payments.append(payment)
         orders.append(order)
 
@@ -185,14 +203,13 @@ def test_confirm_payment_lower_amount(
 TEST_WRONG_CURRENCY = [
     {
         "currency": "DAI - Goerli",
-        "amount": int("1000", base=10),
+        "amount": int("1000", base=10)
     },  # Has enough amount, but in ETH
     {
         "currency": "ETH - Goerli",
-        "amount": int("1000", base=10),
+        "amount": int("1000", base=10)
     },  # Has enough amount, but in DAI
 ]
-
 
 @pytest.mark.django_db(
     transaction=True,
